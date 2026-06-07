5dce98870d3d98361c46250b90403aefcf470cae
lskld <theo.lillieskold@gmail.com>
2026-06-07 20:50:02 +0200

    implement function to list all current snippets in main func

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/lskld/quippet/internal/storage"
+)
 
 func main() {
 	fmt.Println("qpt starting")
+}
+
+func listSnippets() error {
+	snippets, err := storage.Load()
+	if err != nil {
+		return err
+	}
+
+	for _, value := range snippets {
+		fmt.Printf("%s | %s | %v\n", value.Id, value.Title, value.Tags)
+	}
+
+	return nil
 }
