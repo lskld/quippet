11fc5d7cd1a002147be6a9233a06748802de2ef9
lskld <theo.lillieskold@gmail.com>
2026-06-12 17:54:21 +0200

    implement the addSnippet function that asks the user for input and saves a new snippet

--- main.go
+++ main.go
@@ -1,14 +1,75 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+	"time"
 
+	"strconv"
+
+	"github.com/lskld/quippet/internal/model"
 	"github.com/lskld/quippet/internal/storage"
 )
 
 func main() {
 	fmt.Println("qpt starting")
 }
+
+func addSnippet() error {
+	//Saves the following variables: title, []tags, content
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Choose a title: ")
+	title, err := reader.ReadString('\n')
+	if err != nil {
+		return err
+	}
+	title = strings.TrimSpace(title)
+
+	fmt.Print("Choose tags, seperate by comma (eg. banana, apple, pear)")
+	tagInput, err := reader.ReadString('\n')
+	if err != nil {
+		return err
+	}
+	tags := strings.Split(tagInput, ",")
+
+	for i := range tags {
+		tags[i] = strings.TrimSpace(tags[i])
+	}
+
+	fmt.Print("Paste your snippet: ")
+	content, err := reader.ReadString('\n')
+	if err != nil {
+		return err
+	}
+	content = strings.TrimSpace(content)
+
+	newSnippet := model.Snippet {
+		ID: strconv.FormatInt(time.Now().UnixNano(), 10),
+		Title: title,
+		Tags: tags,
+		Content: content,
+		CreatedAt: time.Now(),
+	}
+
+	snippets, err := storage.Load()
+	if err != nil {
+		return err
+	}
+
+	snippets = append(snippets, newSnippet)
+
+	err = storage.Save(snippets)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 
 func listSnippets() error {
 	snippets, err := storage.Load()
@@ -17,7 +78,7 @@ func listSnippets() error {
 	}
 
 	for _, value := range snippets {
-		fmt.Printf("%s | %s | %v\n", value.Id, value.Title, value.Tags)
+		fmt.Printf("%s | %s | %v\n", value.ID, value.Title, value.Tags)
 	}
 
 	return nil
